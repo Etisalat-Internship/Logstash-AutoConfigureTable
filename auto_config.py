f88eb69ddfc403c09e8474481d954256a444efcd
zerkclown <zerkclown@gmail.com>
2018-07-29 21:01:54 +0400

    version 2

--- auto_config.py
+++ auto_config.py
@@ -1,12 +1,12 @@
 import re
 
 # get names of files in python 3.6
-# data_file_name = input("Data File Name: ")
-# conf_file_name = input("Configurations File Name: ")
+data_file_name = input("Data File Name: ")
+conf_file_name = input("Configurations File Name: ")
 
 # get names of files in python 2.7
-data_file_name = raw_input("Data File Name: ")
-conf_file_name = raw_input("Configurations File Name: ")
+# data_file_name = raw_input("Data File Name: ")
+# conf_file_name = raw_input("Configurations File Name: ")
 
 # get column title names
 with open(data_file_name, "r") as in_data:
